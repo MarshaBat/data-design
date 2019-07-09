73c991d5abe5b3ff7a2fc5e582083c13672d3aaf
Marsha Battee <marsha@sanesuite.com>
2019-07-09 16:47:11 -0600

    added links to data design index page

---
+++ epic/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Data Design Project - Marsha Battee</title>
+	</head>
+	<body>
+		<h1>Phase 1 Assignment</h1>
+		<h2>Instagram Data Design</h2>
+		<p>The following files in this project:</p>
+		<ul>
+			<li><a href ="persona-instagram.php">Persona</a></li>
+			<li><a href ="index.php">User Story</a></li>
+			<li><a href ="use-case.php">Use Case/Interaction Flow</a></li>
+			<li><a href="conceptual-model.html">Conceptual Model</a></li>
+		</ul>
+	</body>
+</html>
--- epic/persona-instagram.php
+++ epic/persona-instagram.php
@@ -2,17 +2,59 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8"/>
-		<title>Data Design Project - Marsha Battee</title>
+		<title>User Persona - Data Design Project</title>
 	</head>
 	<body>
-		<h1>Phase 1 Assignment</h1>
-			<h2>Instagram Data Design</h2>
-				<p>The following files in this project:</p>
-					<ul>
-						<li>Persona</li>
-						<li>User Story</li>
-						<li>Use Casee/Interaction Flow</li>
-						<li>Conceptual Model</li>
-					</ul>
+		<h1>User Persona - Data Design Project</h1>
+			<h2>Phase 1 Assignment</h2>
+
+					<div>
+						<h3>Fashionista Frankie - Demographics</h3>
+							<ul>
+								<li>Age: 28</li>
+								<li>Location: New Orleans, LA</li>
+								<li>Education: Marketing/Public Relations</li>
+								<li>Job: Party Promoter</li>
+								<li>Family: Lives with 2 Roommates</li>
+
+							</ul>
+					</div>
+
+					<div>
+						<h3>Goals</h3>
+							<ul>
+								<li>Move to New York City</li>
+								<li>Sure a job in retail marketing at a notable clothing design firm</li>
+								<li>Become an online influencer for fashion</li>
+								<li>Gain 50k followers in one year</li>
+							</ul>
+					</div>
+
+					<div>
+						<h3>Fashion Habits</h3>
+							<ul>
+								<li>Compulsive shopper, especially expensive, glamorous name brands</li>
+								<li>Spends more than her budget allows on clothing. Everything is "must-have"</li>
+								<li>Doesn't do thrift or consignment stores</li>
+							</ul>
+					</div>
+
+					<div>
+						<h3>Tech</h3>
+							<ul>
+								Internet: Excellent
+								Social Network: Only Instagram, but a Star.
+								Messaging: Nope.
+								Games: Not even...
+								Online Shopping: Will be the death of her!
+							</ul>
+					</div>
+
+					<div>
+						<h3>Frustrations</h3>
+							<ul>
+								<li>Anything that doesn't entail shopping</li>
+							</ul>
+					</div>
 	</body>
 </html>
