c0b2cd581fe586a15867897722c410959078950a
Marsha Battee <marsha@sanesuite.com>
2019-07-09 15:48:00 -0600

    text for data design index page

---
+++ epic/persona-instagram.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Data Design Project - Marsha Battee</title>
+	</head>
+	<body>
+		<h1>Phase 1 Assignment</h1>
+			<h2>Instagram Data Design</h2>
+				<p>The following files in this project:</p>
+					<ul>
+						<li>Persona</li>
+						<li>User Story</li>
+						<li>Use Casee/Interaction Flow</li>
+						<li>Conceptual Model</li>
+					</ul>
+	</body>
+</html>
