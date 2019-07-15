0761aa3d6294bc6540dd606ba7837805d845d423
Marsha Battee <marsha@sanesuite.com>
2019-07-15 16:37:29 -0600

    corrected typos

--- sql/data-design.sql
+++ sql/data-design.sql
@@ -1,11 +1,11 @@
--- The statement below sets the collation of the database to utf8
+-- The statement below sets the collation of the database to utf8.
 ALTER DATABASE mbattee CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
 -- These statements will drop the following tables if they exist.
 DROP TABLE IF EXISTS photo;
 DROP TABLE IF EXISTS profile;
 
--- These statements will create the table "profile";
+-- These statements will create the table "profile".
 CREATE TABLE profile (
 	-- The following creates an attribute for the primary key.
 	-- All attributes are required as indicated by NOT NULL.
@@ -15,11 +15,11 @@ CREATE TABLE profile (
 	profileEmail VARCHAR(128) NOT NULL,
 	UNIQUE (profileId),
 	UNIQUE (profileEmail),
-	-- This primary key.
+	-- This is the primary key.
 	PRIMARY KEY (profileId)
 );
 
--- These statements will create the table "photo";
+-- These statements will create the table "photo".
 CREATE TABLE photo (
 	-- The following creates an attribute for the primary key.
 	-- All attributes are required as indicated by NOT NULL.
@@ -29,7 +29,7 @@ CREATE TABLE photo (
 	photoContent VARCHAR(3600) NOT NULL,
 	photoDate DATETIME(6) NOT NULL,
 	photoTime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	-- This primary key.
+	-- This is my primary key.
 	PRIMARY KEY (photoId),
 	FOREIGN KEY	(photoProfileId) REFERENCES profile(profileId)
 );
